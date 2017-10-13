a2a483bd107e74a40d30cf8f0e471955ec4353fe
Patrick Chilton <chpatrick@gmail.com>
2017-10-13 20:54:35 +0200

    Reorganize cipher tests a bit.

--- test/Crypto/Boring/Test/Symmetric.hs
+++ test/Crypto/Boring/Test/Symmetric.hs
@@ -27,22 +27,29 @@ instance IsCipher cipher => Arbitrary (IV cipher) where
   arbitrary = 
     fmap (IV . BS.pack) $ replicateM (cipherBlockSize (untag (reflectCipher @cipher))) arbitrary
 
-roundTrip :: forall cipher. IsCipher cipher => Proxy cipher -> BlockCipherMode -> PaddingMode -> Key cipher -> IV cipher -> Gen Property
-roundTrip _ blockCipherMode paddingMode key iv = do
-  let conf = CipherConfig
-        { ccCipherMode = blockCipherMode
-        , ccPaddingMode = paddingMode
-        , ccKey = key
-        , ccIV = iv
-        }
-  let genChunk = case paddingMode of
-        DisablePadding -> do
-          let blockSize = cipherBlockSize $ untag (reflectCipher @cipher)
-          fmap BS.pack $ replicateM blockSize arbitrary
-        EnablePadding -> BS.pack <$> arbitrary 
-  chunks <- listOf genChunk
-  roundTripChunks <- CL.sourceList chunks $= encrypt conf $= decrypt conf $$ CL.consume
-  return (BS.concat chunks === BS.concat roundTripChunks)
+roundTrip :: forall cipher. IsCipher cipher => Proxy cipher -> BlockCipherMode -> PaddingMode -> Key cipher -> IV cipher -> Property
+roundTrip _ blockCipherMode paddingMode key iv =
+  forAll genChunks $ \chunks ->
+    let
+      roundTripChunks = runConduitPure (CL.sourceList chunks .| encrypt conf .| decrypt conf .| CL.consume)
+    in BS.concat chunks === BS.concat roundTripChunks
+  where
+    conf = CipherConfig
+      { ccCipherMode = blockCipherMode
+      , ccPaddingMode = paddingMode
+      , ccKey = key
+      , ccIV = iv
+      }
+    genChunk = BS.pack <$> arbitrary
+    genChunksUnpadded = listOf genChunk
+    genChunks = case paddingMode of
+      DisablePadding -> do
+        chunks <- genChunksUnpadded
+        let blockSize = cipherBlockSize $ untag (reflectCipher @cipher)
+        let totalSize = sum $ map BS.length chunks
+        extraChunk <- BS.pack <$> replicateM (blockSize - (totalSize `mod` blockSize)) arbitrary
+        return (chunks ++ [ extraChunk ])
+      EnablePadding -> genChunksUnpadded
 
 symmetricTests :: TestTree
 symmetricTests = testGroup "Symmetric" $ do
